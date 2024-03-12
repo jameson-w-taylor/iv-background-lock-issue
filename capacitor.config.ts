352fc3e2693802a7ac8a5a3405f4ffe003a37fe3
Jameson Taylor <jameson@ionic.io>
2024-03-12 13:11:53 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iv-background-lock-issue',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
